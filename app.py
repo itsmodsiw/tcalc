698f755eaf157e9dd9467bc65d6332bcb802c5e2
modsiw <modsiw@gmail.com>
2023-07-15 23:37:58 +0200

    Unix Time Converter

--- app.py
+++ app.py
@@ -7,30 +7,38 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        timezone = request.form.get('timezone')
-        date = request.form.get('date')
-        time = request.form.get('time')
+        try:
+            timezone = request.form.get('timezone')
+            date = request.form.get('date')
+            time = request.form.get('time')
 
-        dt = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
-        dt = pytz.timezone(timezone).localize(dt)
+            dt = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
+            dt = pytz.timezone(timezone).localize(dt)
 
-        unix_time = int(dt.timestamp())
+            unix_time = int(dt.timestamp())
 
-        return render_template('result.html', unix_time=unix_time)
+            return render_template('result.html', unix_time=unix_time)
+        except Exception as e:
+            error_message = "An error occurred during the conversion: " + str(e)
+            return render_template('error.html', error_message=error_message)
 
     return render_template('index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    data = request.get_json()  # Get the JSON data sent from the frontend
-    datetime_str = data['datetime']  # Extract the datetime string
+    try:
+        data = request.get_json()  # Get the JSON data sent from the frontend
+        datetime_str = data['datetime']  # Extract the datetime string
 
-    # Parse the datetime string and convert to Unix timestamp...
-    dt = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M")
-    unix_timestamp = int(dt.timestamp())
+        # Parse the datetime string and convert to Unix timestamp...
+        dt = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M")
+        unix_timestamp = int(dt.timestamp())
 
-    # Send the Unix timestamp back to the frontend as JSON
-    return jsonify({'timestamp': unix_timestamp})
+        # Send the Unix timestamp back to the frontend as JSON
+        return jsonify({'timestamp': unix_timestamp})
+    except Exception as e:
+        error_message = "An error occurred during the conversion: " + str(e)
+        return jsonify({'error': error_message}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
