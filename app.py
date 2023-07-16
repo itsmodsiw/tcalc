0b15acdcfefd37836c1f0636723745c8245b95a1
modsiw <modsiw@gmail.com>
2023-07-16 12:14:23 +0200

    updating cacl
    Thanks to Noah feedback, we added some cool stuff.

--- app.py
+++ app.py
@@ -4,44 +4,25 @@ import pytz
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        try:
-            timezone = request.form.get('timezone')
-            date = request.form.get('date')
-            time = request.form.get('time')
-
-            dt = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
-            dt = pytz.timezone(timezone).localize(dt)
-
-            unix_time = int(dt.timestamp())
-
-            return render_template('result.html', unix_time=unix_time)
-        except Exception as e:
-            error_message = "An error occurred during the conversion: " + str(e)
-            return render_template('error.html', error_message=error_message)
-
     return render_template('index.html')
-
 
 @app.route('/convert', methods=['POST'])
 def convert():
     try:
-        data = request.get_json()  # Get the JSON data sent from the frontend
-        datetime_str = data['datetime']  # Extract the datetime string
+        data = request.get_json()
+        datetime_str = data['datetime']
+        timezone_str = data['timezone']
 
-        # Parse the datetime string and convert to Unix timestamp...
         dt = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M")
+        dt = pytz.timezone(timezone_str).localize(dt)
         unix_timestamp = int(dt.timestamp())
 
-        # Send the Unix timestamp back to the frontend as JSON
         return jsonify({'timestamp': unix_timestamp})
     except Exception as e:
         error_message = "An error occurred during the conversion: " + str(e)
         return jsonify({'error': error_message}), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True)
